2862843cde3b41f7b0e3f91ab6ee62c2a98023dc
Mohit Patel <mohit.patel1966@gmail.com>
2022-11-17 00:45:28 +0530

    did very smart optimizing in theme toggle

--- theme.js
+++ theme.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const switchTheme = () => {
-        if(themePreferenceDark.matches){
-            document.body.classList.toggle("dark-theme");
-        } else {
             document.body.classList.toggle("light-theme");
-        }
+            themeSwitcher.textContent = (document.body.classList.value === "light-theme") ? "Dark" : "Light" ;
     }
 
     const themeSwitcher = document.getElementById("theme-change-button");
-    const themePreferenceDark = window.matchMedia("prefers-color-scheme: dark");
     themeSwitcher.addEventListener("click" , switchTheme);
-    themeSwitcher.textContent = themePreferenceDark.matches ? "Light" : "Dark" ;
-
+    themeSwitcher.textContent = (document.body.classList.value === "light-theme") ? "Dark" : "Light" ;
 })
