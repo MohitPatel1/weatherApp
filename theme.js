597ae8b66c21e553d44a51fb9492e0f2494f23a2
Mohit Patel <mohit.patel1966@gmail.com>
2022-12-13 14:31:34 +0530

    border color added to toggle button

--- theme.js
+++ theme.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const switchTheme = () => {
             document.body.classList.toggle("light-theme");
             const themeButton = document.querySelector(".theme-change-button")
-            console.log(themeButton.src);
             themeButton.src = (document.body.classList.contains("light-theme")) ? "images/toggle_light.png" : "images/toggle_dark.png" ;
+            themeButton.style.borderColor = (document.body.classList.contains("light-theme")) ? "black" : "White" ;
     }
 
     const themeSwitcher = document.querySelector(".theme-change-button");
